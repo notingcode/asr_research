df5f42ace1c63fa366dd3df627bd18193673f428
notingcode <notingcode@gmail.com>
2023-05-04 18:37:11 +0900

    extract zip can strip directories

--- untar_unzip.py
+++ untar_unzip.py
@@ -8,7 +8,7 @@ from pathlib import Path
 import torchaudio
 
 
-def _extract_tar(from_path: str, to_path: Optional[str] = None, overwrite: bool = False, n_directories_stripped: int = 9,) -> List[str]:
+def _extract_tar(from_path: str, to_path: Optional[str] = None, overwrite: bool = False, n_directories_stripped: int = 9,) -> List[tarfile.TarInfo]:
     if to_path is None:
         to_path = os.path.dirname(from_path)
     with tarfile.open(from_path, "r") as tar:
@@ -19,7 +19,7 @@ def _extract_tar(from_path: str, to_path: Optional[str] = None, overwrite: bool 
             file_path = os.path.join(to_path, member.path)
             if os.path.exists(file_path):
                 logging.info("{} already extracted.".format(file_path))
-                if overwrite:
+                if not overwrite:
                     continue
             files.append(member)
             
@@ -27,20 +27,21 @@ def _extract_tar(from_path: str, to_path: Optional[str] = None, overwrite: bool 
         return files
 
 
-def _extract_zip(from_path: str, to_path: Optional[str] = None, overwrite: bool = False, n_directories_stripped: int = 0,) -> List[str]:
+def _extract_zip(from_path: str, to_path: Optional[str] = None, overwrite: bool = False, n_directories_stripped: int = 0,) -> List[zipfile.ZipInfo]:
     if to_path is None:
         to_path = os.path.dirname(from_path)
 
     with zipfile.ZipFile(from_path, "r") as zfile:
         logging.info("Opened zip file {}.", from_path)
         files = []
-        for file_ in zfile.namelist():
-            file_path = os.path.join(to_path, file_)
-            if os.path.exists(file_path):
-                logging.info("{} already extracted.".format(file_path))
+        for file_info in zfile.infolist():
+            file_info.filename = file_info.filename.split('/', n_directories_stripped)[-1]
+            file_info.filename = os.path.join(to_path, file_info.filename)
+            if os.path.exists(file_info.filename):
+                logging.info("{} already extracted.".format(file_info.filename))
                 if not overwrite:
                     continue
-            files.append(file_)
+            files.append(file_info)
             
         zfile.extractall(to_path, members=files)
     return files
