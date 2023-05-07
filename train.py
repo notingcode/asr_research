42b40d7cd0be6f41c6a42ba149dc560313040bbe
notingcode <notingcode@gmail.com>
2023-05-08 02:02:52 +0900

    try tpu for colab

--- train.py
+++ train.py
@@ -38,7 +38,7 @@ def run_train(args):
         max_epochs=args.epochs,
         num_nodes=args.nodes,
         accelerator="tpu",
-        strategy="auto",
+        strategy="xla",
         callbacks=callbacks,
         reload_dataloaders_every_n_epochs=1,
         gradient_clip_val=10.0,
