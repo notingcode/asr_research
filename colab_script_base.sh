dd36d736e6e91530a12aaa0b95a88692fb5bcdba
notingcode <notingcode@gmail.com>
2023-05-11 01:12:30 +0900

    added validation label files for testing on colab

--- colab_script_base.sh
+++ colab_script_base.sh
@@ -3,6 +3,7 @@ cp "/content/drive/MyDrive/asr_research/한국인 대화
 cp "/content/drive/MyDrive/asr_research/한국인 대화 음성/Training/[원천]3.일상안부_dialog_01.tar" "./한국인 음성/Training" &
 # cp "/content/drive/MyDrive/asr_research/한국인 대화 음성/Training/[원천]7.놀이_play_01.tar" "./한국인 음성/Training" &
 
+cp "/content/drive/MyDrive/asr_research/한국인 대화 음성/Validation/[라벨]3.일상안부_dialog_01.tar.gz" "./한국인 음성/Validation" &
 # cp "/content/drive/MyDrive/asr_research/한국인 대화 음성/Validation/[원천]2.취미_hobby_01.tar.gz" "./한국인 음성/Validation" &
 # cp "/content/drive/MyDrive/asr_research/한국인 대화 음성/Validation/[원천]7.놀이_play_01.tar.gz" "./한국인 음성/Validation" &
 cp "/content/drive/MyDrive/asr_research/한국인 대화 음성/Validation/[원천]3.일상안부_dialog_01.tar.gz" "./한국인 음성/Validation" &
