4edfdf6424b47c2f9e258a1db70e1279e26e1b10
notingcode <notingcode@gmail.com>
2023-05-15 14:11:59 +0900

    support tokenization of diquest normal speech

--- train_spm_base.py
+++ train_spm_base.py
@@ -7,104 +7,60 @@ python train_spm.py --kor-scripts-path ./datasets
 
 import io
 import pathlib
-import numpy as np
-import re
+import json
 from argparse import ArgumentParser, RawTextHelpFormatter
+from script_normalization import(
+    cleanup_transcript,
+    edit_annotation,
+)
 
 import sentencepiece as spm
 
-def get_transcript_text(transcript_path, sep):
+SCRIPTS_TXT_EXT = "*_scripts.txt"
+JSON_EXT = "*.json"
+TXT_EXT = "*.txt"
+
+EXT_DICT = {"txt": TXT_EXT, "json": JSON_EXT, "scripts": SCRIPTS_TXT_EXT}
+
+def get_scripts_from_txt(transcript_path, separator):
     new_list = list()
     with open(transcript_path) as f:
         for line in f:
-            modified_line = cleanup_transcript(line.split(sep, 1)[-1].strip())
-            if len(modified_line) != 0:
+            modified_line = cleanup_transcript(line.split(separator, 1)[-1].strip())
+            if modified_line is not None:
                 new_list.append(modified_line)
         return new_list
+    
+def get_script_from_json(transcript_path):
+    with open(transcript_path) as f:
+        data = json.load(f)
+        modified_line = edit_annotation(data['발화정보']['stt'].strip("\\"))
+        
+    return modified_line
 
+def get_script_from_txt(transcript_path, separator):
+    with open(transcript_path) as f:
+        modified_line = cleanup_transcript(f.readline().split(separator, 1)[-1].strip())
+        
+    return modified_line
 
-def get_transcripts(dataset_path, sep):
+def get_transcripts(dataset_path, typ: str="txt", separator: str=""):
+    
+    ext = EXT_DICT[typ]
+    
     training_metadata_base_path = pathlib.PosixPath(dataset_path)
-    training_scripts_filePath = training_metadata_base_path.rglob("*_scripts.txt")
+    training_scripts_filePath = training_metadata_base_path.rglob(ext)
     merged_transcripts = []
     for path in training_scripts_filePath:
-        merged_transcripts += get_transcript_text(path, sep)
+        match typ:
+            case "scripts": merged_transcripts += get_scripts_from_txt(path, separator)
+            case "json": merged_transcripts.append(get_script_from_json(path))
+            case "txt": merged_transcripts.append(get_script_from_txt(path, separator))
+
     return merged_transcripts
 
-
-def cleanup_transcript(line_of_text) -> str:
-    if(len(line_of_text) < 5):
-        return ""
-    
-    cond = False
-    partitions = re.split("(\([^\)]*\)/\([^\)]*\))", line_of_text)
-    if(len(partitions) != 1):
-        for part in partitions:
-            curr = re.findall("\(([^\)]*)\)", part)
-            for element in curr:
-                if(('(' in element) or (')' in element)):
-                    cond = True
-                    break
-    if cond == True:
-        return ""
-
-    left = line_of_text.count('(')
-    right = line_of_text.count(')')
-    
-    if(left != right or left%2 == 1):
-        return ""
-
-    line_of_text = spelling_rep(line_of_text)
-    
-    if(bool(re.search('\d', line_of_text))):
-        return ""
-    
-    if('(' in line_of_text or ')' in line_of_text):
-        return ""
-
-    line_of_text = remove_special_words(line_of_text)
-    line_of_text = remove_dup_spaces(line_of_text)
-    
-    left = line_of_text.count('(')
-    right = line_of_text.count(')')
-    
-    if(left != right or left%2 == 1):
-        return ""
-    
-    return line_of_text
-
-
-def spelling_rep(line_of_transcript):
-    result = ""
-    
-    segment_list = re.split("(\([^\)]*\)/\([^\)]*\))", line_of_transcript)
-    if(len(segment_list) != 1):
-        for segment in segment_list:
-            curr = re.findall("\(([^\)]*)\)", segment)
-            if(len(curr) == 0):
-                result += segment
-            else:
-                if(bool(re.search('\d', curr[0]))):
-                    result += curr[1]
-                else:
-                    result += curr[0]
-        return result
-    
-    return line_of_transcript
-
-
-def remove_special_words(x):
-    special_symbol = re.compile("[\*\+/blon\.\?]")
-    return re.sub(special_symbol, "", x)
-
-
-def remove_dup_spaces(x):
-    special_symbol = re.compile(r"\s+")
-    return re.sub(special_symbol, " ", x).strip()
-
-
 def train_spm(input_file, prefix):
-    spm.SentencePieceTrainer.train(
+    spm.SentencePieceTrainer.Train(
         input = input_file,
         model_prefix = prefix,
         vocab_size=6000,
