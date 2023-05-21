34074daf58db34af149d0ad07710f0aea6a46f52
notingcode <notingcode@gmail.com>
2023-05-21 14:39:27 +0900

    unpack function complete

--- diquest_normalspeech.py
+++ diquest_normalspeech.py
@@ -5,62 +5,40 @@ from typing import Tuple, Union
 
 from torch import Tensor
 from torch.utils.data import Dataset
-from untar_unzip import _extract_tar, _load_waveform
+from untar_unzip import _extract_zip, _load_waveform
 from train_spm_base import cleanup_transcript
-from common import SAMPLE_RATE
+from common import(
+    SAMPLE_RATE,
+    TRAIN_SUBDIR_NAME,
+    VALID_SUBDIR_NAME,
+)
 
-N_DIRECTORIES_STRIPPED = 9
-_DATA_SUBSETS = [
-    "broadcast",
-    "hobby",
-    "dialog",
-    "life",
-    "weather",
-    "economy",
-    "play",
-    "shopping",
-    "all"
-]
-TRAIN_SUBDIR_NAME = "Training"
-VALID_SUBDIR_NAME = "Validation"
-SUBDIR_GETTER = 100000
-INDEX_SUBDIR_GETTER = 1000
+N_DIRECTORIES_STRIPPED = 0
+SUBDIR_HEADER = "일반남여"
 
-def _unpack_korConverseSpeech(source_path: Union[str, Path], subset_type: str):
-    ext_archive = ".tar"
+def unpack_diquestSpeech(source_path: Union[str, Path]):
+    ext_archive = ".zip"
         
-    if subset_type == "all":
-        tar_files = Path(source_path).glob(f"*{ext_archive}*")
-    else:
-        tar_files = Path(source_path).glob(f"*{subset_type}_*{ext_archive}*")
+    zip_files = Path(source_path).glob(f"*{ext_archive}")
     
     args = []
     
-    for file in tar_files:
+    for file in zip_files:
         args.append((file.as_posix(), source_path, False, N_DIRECTORIES_STRIPPED))
     
     pool = mp.Pool(min(mp.cpu_count(), len(args)))
     
-    pool.starmap(_extract_tar, args, chunksize=1)
+    pool.starmap(_extract_zip, args, chunksize=1)
 
 
 def _get_korConverseSpeech_metadata(
     filename: str, dataset_path: str, ext_audio: str, ext_txt: str,
 ) -> Tuple[str, int, str]:
-    subset_type, index = filename.split("_")
 
-    index = int(index)-1
 
-    transcript_file = filename+ext_txt
-    audio_file = filename+ext_audio
-    subset_subdir = f"{subset_type}_{(index//SUBDIR_GETTER)+1:02d}"
-    indexed_dir = f"{(index//INDEX_SUBDIR_GETTER)+1:03d}"
-    
-    transcript_filepath = os.path.join(dataset_path, subset_subdir, indexed_dir, transcript_file)
-    audio_filepath = os.path.join(dataset_path, subset_subdir, indexed_dir, audio_file)
 
     # Load text
-    with open(transcript_filepath) as f:
+    with open(filename) as f:
         transcript = cleanup_transcript(f.readline().strip())
         if transcript is None:
             # Translation not found
@@ -73,7 +51,7 @@ def _get_korConverseSpeech_metadata(
     )
 
 
-class KORCONVERSESPEECH(Dataset):
+class DIQUESTSPEECH(Dataset):
     """
     Args:
         root (str or Path): Path to the directory where the dataset is found or downloaded.
@@ -99,21 +77,16 @@ class KORCONVERSESPEECH(Dataset):
             
         self.dataset_path = dataset_path
 
-        assert(self.subset_type in _DATA_SUBSETS)
-
-        _unpack_korConverseSpeech(self.dataset_path, self.subset_type)
+        unpack_diquestSpeech(self.dataset_path)
         
-        if self.subset_type == "all":
-            audio_files_path = Path(self.dataset_path).rglob("*"+self._ext_audio)
-        else:
-            audio_files_path = Path(self.dataset_path).rglob(f"{subset_type}_*"+self._ext_audio)
+        audio_files_path = Path(self.dataset_path).rglob("*"+self._ext_audio)
         
         self._walker = []
 
         for path in audio_files_path:
             with open(path.with_suffix(self._ext_txt)) as f:
                 transcript = cleanup_transcript(f.readline().strip())
-                if len(transcript) > 0:
+                if transcript is not None:
                     self._walker.append(path.stem)                        
 
     def get_metadata(self, n: int) -> Tuple[str, int, str]:
