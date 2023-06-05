9278e11df4d2270dd9d547a1dc6514e0a4a9daed
notingcode <notingcode@gmail.com>
2023-06-05 13:14:42 +0900

    test on one dataset

--- colab_train_asr.sh
+++ colab_train_asr.sh
@@ -18,36 +18,36 @@ mkdir -p "$DATASETS_PATH/$DATASET3/$DATASET3_SUBDIR/1.$TRAINING/$DATASET3_SRC"
 mkdir -p "$DATASETS_PATH/$DATASET3/$DATASET3_SUBDIR/2.$VALIDATION/$DATASET3_LABEL"
 mkdir -p "$DATASETS_PATH/$DATASET3/$DATASET3_SUBDIR/2.$VALIDATION/$DATASET3_SRC"
 
-#Dataset 1 Label
-#Train
-cp "$GOOGLE_DRIVE_PATH/$DATASET1/$TRAINING/[라벨]3.일상안부_dialog_01.tar.gz" "$DATASETS_PATH/$DATASET1/$TRAINING" &
+# #Dataset 1 Label
+# #Train
+# cp "$GOOGLE_DRIVE_PATH/$DATASET1/$TRAINING/[라벨]3.일상안부_dialog_01.tar.gz" "$DATASETS_PATH/$DATASET1/$TRAINING" &
 
-#Validation
-cp "$GOOGLE_DRIVE_PATH/$DATASET1/$VALIDATION/[라벨]3.일상안부_dialog_01.tar.gz" "$DATASETS_PATH/$DATASET1/$VALIDATION" &
+# #Validation
+# cp "$GOOGLE_DRIVE_PATH/$DATASET1/$VALIDATION/[라벨]3.일상안부_dialog_01.tar.gz" "$DATASETS_PATH/$DATASET1/$VALIDATION" &
 
-#Dataset 1 Source
-#Train
-cp "$GOOGLE_DRIVE_PATH/$DATASET1/$TRAINING/[원천]3.일상안부_dialog_01.tar" "$DATASETS_PATH/$DATASET1/$TRAINING" &
+# #Dataset 1 Source
+# #Train
+# cp "$GOOGLE_DRIVE_PATH/$DATASET1/$TRAINING/[원천]3.일상안부_dialog_01.tar" "$DATASETS_PATH/$DATASET1/$TRAINING" &
 
-#Validation
-cp "$GOOGLE_DRIVE_PATH/$DATASET1/$VALIDATION/[원천]3.일상안부_dialog_01.tar.gz" "$DATASETS_PATH/$DATASET1/$VALIDATION" &
-cp "$GOOGLE_DRIVE_PATH/$DATASET1/$VALIDATION/[원천]4.생활_life_01.tar.gz" "$DATASETS_PATH/$DATASET1/$VALIDATION" &
+# #Validation
+# cp "$GOOGLE_DRIVE_PATH/$DATASET1/$VALIDATION/[원천]3.일상안부_dialog_01.tar.gz" "$DATASETS_PATH/$DATASET1/$VALIDATION" &
+# cp "$GOOGLE_DRIVE_PATH/$DATASET1/$VALIDATION/[원천]4.생활_life_01.tar.gz" "$DATASETS_PATH/$DATASET1/$VALIDATION" &
 
-###################################################################################################################################################
+# ###################################################################################################################################################
 
-#Dataset 2 Label
-#Train
-cp "$GOOGLE_DRIVE_PATH/$DATASET2/$TRAINING/[라벨]3.스튜디오.zip" "$DATASETS_PATH/$DATASET2/$TRAINING" &
+# #Dataset 2 Label
+# #Train
+# cp "$GOOGLE_DRIVE_PATH/$DATASET2/$TRAINING/[라벨]3.스튜디오.zip" "$DATASETS_PATH/$DATASET2/$TRAINING" &
 
-#Validation
-cp "$GOOGLE_DRIVE_PATH/$DATASET2/$VALIDATION/[라벨]3.스튜디오.zip" "$DATASETS_PATH/$DATASET2/$VALIDATION" &
+# #Validation
+# cp "$GOOGLE_DRIVE_PATH/$DATASET2/$VALIDATION/[라벨]3.스튜디오.zip" "$DATASETS_PATH/$DATASET2/$VALIDATION" &
 
-#Dataset 2 Source
-#Train
-cp "$GOOGLE_DRIVE_PATH/$DATASET2/$TRAINING/[원천]3.스튜디오_1.zip" "$DATASETS_PATH/$DATASET2/$TRAINING" &
+# #Dataset 2 Source
+# #Train
+# cp "$GOOGLE_DRIVE_PATH/$DATASET2/$TRAINING/[원천]3.스튜디오_1.zip" "$DATASETS_PATH/$DATASET2/$TRAINING" &
 
-#Validation
-cp "$GOOGLE_DRIVE_PATH/$DATASET2/$VALIDATION/[원천]3.스튜디오.zip" "$DATASETS_PATH/$DATASET2/$VALIDATION" &
+# #Validation
+# cp "$GOOGLE_DRIVE_PATH/$DATASET2/$VALIDATION/[원천]3.스튜디오.zip" "$DATASETS_PATH/$DATASET2/$VALIDATION" &
 
 ###################################################################################################################################################
 
